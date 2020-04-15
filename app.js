c055a82266fe982cfcb87b465d0f90cbfed377d7
Luiz <luiz.felipe.556@outlook.com>
2020-04-15 14:01:28 -0300

    class 32

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-    console.log('2 SECOND TIMER');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=0a8bf0e59c293b5ff2364743ca0521ad&query=37.8267,-122.4233&units=f'
 
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0)
-
-console.log('Stopping');
+request({ url: url, json: true }, (error, response) => {
+    const temperature = response.body.current.temperature;
+    const feelsLike = response.body.current.feelslike;
+    const weatherDes = response.body.current.weather_descriptions[0];
+    console.log(weatherDes + '. It is currently ' + temperature + ' degress out. It feels like ' + feelsLike + ' degress out.');
+});
