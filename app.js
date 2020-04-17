51bd70df4e070d96eba7bd07bacdf22125be8cd9
Luiz <luiz.felipe.556@outlook.com>
2020-04-17 12:15:04 -0300

    class 38

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Kampala', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
 
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error', error);
-        console.log('Data', data);
+if (process.argv[2] && process.argv[2].length > 0) {
+
+geocode(process.argv[2], (error, data) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        console.log(data.location);
+        console.log(forecastData);
+
     })
 })
+
+} else {
+    console.log('Please provide a location!');
+}
